306cdcda83a5877d78f4101c528fa8a884b75771
raul.al.go96 <raul.al.go96@gmail.com>
2025-03-04 19:48:26 -0700

    First lesson!

---
+++ 2_docker_sql/pipeline.py
@@ -0,0 +1,19 @@
+import sys
+
+import pandas as pd
+
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info) 
+
+print(sys.argv)
+
+
+day = sys.argv[1]
+
+#
+print("Hello World")
+
+
+print("job done for day = {day}")
